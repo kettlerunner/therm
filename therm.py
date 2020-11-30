4ea962828b6c8a4a592ea1394f2a7d15f42f733b
KettleRunner <crousetech@gmail.com>
2020-11-30 09:08:35 -0600

    Update therm.py

--- therm.py
+++ therm.py
@@ -42,7 +42,7 @@ def draw_label(img, text, pos, bg_color):
 
 #out = cv2.VideoWriter('therm.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (800,480))
 
-alpha = 7.1
+alpha = 6.1
 status = "reading"
 face_in_frame = False
 cap = cv2.VideoCapture(0)
@@ -126,7 +126,8 @@ while(True):
                 port = 25  # For starttls
                 smtp_server = "mail.precisionathleticswi.com"
                 sender_email = "thermy@precisionathleticswi.com"
-                receiver_email = "jamie.spoor@compass-usa.com"
+                receiver_email = "dan@precisionathleticswi.com"
+                #receiver_email = "jamie.spoor@compass-usa.com"
                 password = "thermy123"
 
                 message = MIMEMultipart("alternative")
