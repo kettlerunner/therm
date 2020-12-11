2563bd1949c68f7e422debc01b979a4956a91936
KettleRunner <crousetech@gmail.com>
2020-12-11 16:31:03 -0600

    Update therm.py

--- therm.py
+++ therm.py
@@ -126,9 +126,9 @@ while(True):
             df = pd.DataFrame()
             df['timestamps'] = [datetime.datetime.utcnow() - datetime.timedelta(hours=6, minutes=0)]
             df['temps'] = [display_temp]
-            df.to_csv('archive.csv', mode='a', header=False)
             df2 = pd.read_csv('archive.csv')
             df2['timestamps'] = df2['timestamps'].astype('datetime64[ns]')
+            df2.append(df, ignore_index = True)
             df2 = df2[df2['timestamps'] > pd.Timestamp((datetime.datetime.utcnow() - datetime.timedelta(hours=6, minutes=0)) - datetime.timedelta(days=1))]
             df2.to_csv('archive.csv')
             if display_temp >= 100.0:
